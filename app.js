229d2dcfdb16c5c77c288886a273f33e2d50e20a
astrolince <juan77.sonic@gmail.com>
2016-04-25 18:36:13 -0300

    forever to pm2

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var http = require('http'),
+'use strict';
+
+let http = require('http'),
   schedule = require('node-schedule'),
   request = require('request'),
   nodemailer = require('nodemailer'),
@@ -25,12 +27,12 @@ var http = require('http'),
   };
 
 respondedorbot.on('message', function (msg) {
-  var chatId = msg.chat.id;
-  var text = msg.text;
-  var opts = {
+  let chatId = msg.chat.id;
+  let text = msg.text;
+  let opts = {
     reply_to_message_id: msg.message_id
   };
-  var random = Math.round(Math.random());
+  let random = Math.round(Math.random());
   if (text.match(/^\//)) {
     if (text.match(/^\/ask/)) {
       if (random === 1) {
@@ -51,16 +53,16 @@ respondedorbot.on('message', function (msg) {
 rulerestart.minute = [25, 55];
 check15.second = [0, 15, 30, 45];
 
-var autorulerestart = schedule.scheduleJob(rulerestart, function(){
+let autorulerestart = schedule.scheduleJob(rulerestart, function(){
   request('http://thegman.herokuapp.com');
   heroku.apps('tuxifeed').dynos().restartAll();
 });
 
-var autocheck15 = schedule.scheduleJob(check15, function(){
+let autocheck15 = schedule.scheduleJob(check15, function(){
   check('TuxiFeed', 'tuxifeed', 'http://tuxifeed.herokuapp.com');
 });
 
-var port = Number(process.env.PORT || 5000);
+let port = Number(process.env.PORT || 5000);
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('The G-Man is watching you.\n');
