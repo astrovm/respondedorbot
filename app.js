423e7e807b52f8d6fad2b5276fc12299d77879e1
astro <astro@astrolince.com>
2018-01-02 07:13:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
 'use strict';
 
-let http = require('http'),
-  schedule = require('node-schedule'),
-  request = require('request'),
+const http = require('http'),
   TelegramBot = require('node-telegram-bot-api'),
   respondedorbot = new TelegramBot(process.env.RESPONDEDORBOT_TELE_TOKEN, { polling: true }),
   secwalbot = new TelegramBot(process.env.SECWALRBOT_TELE_TOKEN, { polling: true }),
   sectmbot = new TelegramBot(process.env.SECTMBOT_TELE_TOKEN, { polling: true }),
   secaybot = new TelegramBot(process.env.SECAYBOT_TELE_TOKEN, { polling: true }),
-  secpubot = new TelegramBot(process.env.SECPUBOT_TELE_TOKEN, { polling: true }),
-  Heroku = require('heroku-client'),
-  heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN }),
-  rulerestart = new schedule.RecurrenceRule(),
-  check15 = new schedule.RecurrenceRule(),
-  check = function (title, app, url) {
-    request(url, function (err, res) {
-      if (!err && res.statusCode == 200) {
-      } else {
-        heroku.apps(app).dynos().restartAll()
-        console.log(title + ' DOWN')
-      };
-    });
-  };
+  secpubot = new TelegramBot(process.env.SECPUBOT_TELE_TOKEN, { polling: true })
 
 respondedorbot.on('message', function (msg) {
-  let chatId = msg.chat.id;
-  let opts = {
+  const chatId = msg.chat.id;
+  const opts = {
     reply_to_message_id: msg.message_id
   };
-  let random = Math.round(Math.random());
+  const random = Math.round(Math.random());
   if (random === 1) {
     respondedorbot.sendMessage(chatId, 'si', opts)
   } else {
@@ -38,76 +23,62 @@ respondedorbot.on('message', function (msg) {
 
 
 secwalbot.on('message', function (msg) {
-  let chatId = msg.chat.id;
-  let opts = {
+  const chatId = msg.chat.id;
+  const opts = {
     reply_to_message_id: msg.message_id
-  };
-  let random = Math.round(Math.random());
+  }
+  const random = Math.round(Math.random());
   if (random === 1) {
     secwalbot.sendMessage(chatId, 'si', opts)
   } else {
     secwalbot.sendMessage(chatId, 'no', opts)
-  };
-});
+  }
+})
 
 sectmbot.on('message', function (msg) {
-  let chatId = msg.chat.id;
-  let opts = {
+  const chatId = msg.chat.id;
+  const opts = {
     reply_to_message_id: msg.message_id
-  };
-  let random = Math.round(Math.random());
+  }
+  const random = Math.round(Math.random());
   if (random === 1) {
     sectmbot.sendMessage(chatId, 'guau si', opts)
   } else {
     sectmbot.sendMessage(chatId, 'guau no', opts)
-  };
-});
+  }
+})
 
 secaybot.on('message', function (msg) {
-  let chatId = msg.chat.id;
-  let opts = {
+  const chatId = msg.chat.id;
+  const opts = {
     reply_to_message_id: msg.message_id
-  };
-  let random = Math.round(Math.random());
+  }
+  const random = Math.round(Math.random());
   if (random === 1) {
     secaybot.sendMessage(chatId, 'miau si', opts)
   } else {
     secaybot.sendMessage(chatId, 'miau no', opts)
-  };
-});
+  }
+})
 
 
 secpubot.on('message', function (msg) {
-  let chatId = msg.chat.id;
-  let opts = {
+  const chatId = msg.chat.id;
+  const opts = {
     reply_to_message_id: msg.message_id
-  };
-  let random = Math.round(Math.random());
+  }
+  const random = Math.round(Math.random());
   if (random === 1) {
     secpubot.sendMessage(chatId, 'RAFRAFAYAYASNJCRA si', opts)
   } else {
     secpubot.sendMessage(chatId, 'RAFRAFAYAYASNJCRA no', opts)
-  };
-});
+  }
+})
 
-rulerestart.minute = [25, 55];
-check15.second = [30];
-
-let autorulerestart = schedule.scheduleJob(rulerestart, function(){
-  request('http://thegman.herokuapp.com')
-  heroku.apps('tuxifeed').dynos().restartAll()
-});
-
-let autocheck15 = schedule.scheduleJob(check15, function(){
-  request('http://thegman.herokuapp.com')
-  request('http://linky.tk')
-  check('TuxiFeed', 'tuxifeed', 'http://tuxifeed.herokuapp.com')
-});
-
-let port = Number(process.env.PORT || 5000);
+const port = Number(process.env.PORT || 5000);
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end('The G-Man is watching you.\n')
 }).listen(port, function() {
   console.log('Listening on ' + port)
-});
+})
