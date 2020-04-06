01e917a68704cf823118b2586bdf21b6a5b6e3b5
astrolince <astro@astrolince.com>
2020-04-06 07:46:00 -0300

    basic error handling

--- main.py
+++ main.py
@@ -20,14 +20,24 @@ def gen_random(name):
 
 
 def responder(request):
-    if request.method == "POST":
-        req = request.get_json()
-        token = str(request.args.get("token"))
-        chat_id = str(req["message"]["chat"]["id"])
-        message_id = str(req["message"]["message_id"])
-        first_name = str(req["message"]["from"]["first_name"])
-        msj = gen_random(first_name)
-        url = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + \
-            chat_id + '&reply_to_message_id=' + message_id + '&text=' + msj
-        get(url)
-    return "ok"
+    try:
+        if request.method == "POST":
+            token = str(request.args.get("token"))
+
+            req = request.get_json()
+            chat_id = str(req["message"]["chat"]["id"])
+            message_id = str(req["message"]["message_id"])
+            first_name = str(req["message"]["from"]["first_name"])
+
+            msj = gen_random(first_name)
+
+            url = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + \
+                chat_id + '&reply_to_message_id=' + message_id + '&text=' + msj
+
+            get(url)
+
+            return "ok"
+        else:
+            return "bad request"
+    except:
+        return "unexpected error"
