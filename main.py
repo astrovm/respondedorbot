fc1f961004b9b9f7e6b0fc7e254956c59377fcd0
astrolince <astro@astrolince.com>
2023-04-03 14:09:24 -0300

    improve verify_webhook

--- main.py
+++ main.py
@@ -729,19 +729,26 @@ def verify_webhook(decrypted_token: str, encrypted_token: str) -> bool:
     webhook_info = get_telegram_webhook_info(decrypted_token)
     main_function_url = environ.get("MAIN_FUNCTION_URL")
     current_function_url = environ.get("CURRENT_FUNCTION_URL")
-    webhook_url = f"{main_function_url}?token={encrypted_token}"
+    main_webhook_url = f"{main_function_url}?token={encrypted_token}"
+    current_webhook_url = f"{current_function_url}?token={encrypted_token}"
+
+    def set_main_webhook() -> bool:
+        return set_telegram_webhook(decrypted_token, main_function_url, encrypted_token)
+
     if main_function_url == current_function_url:
-        return webhook_info.get("url", "") == webhook_url or set_telegram_webhook(decrypted_token, main_function_url, encrypted_token)
+        return webhook_info.get("url") == main_webhook_url or set_main_webhook()
+
     try:
         function_response = requests.get(main_function_url, timeout=5)
         function_response.raise_for_status()
     except RequestException as request_error:
-        if webhook_info.get("url", "") != f"{current_function_url}?token={encrypted_token}":
+        if webhook_info.get("url") != current_webhook_url:
             error_message = f"Main webhook failed with error: {str(request_error)}"
             admin_report(decrypted_token, error_message)
             return set_telegram_webhook(decrypted_token, current_function_url, encrypted_token)
         return True
-    return webhook_info.get("url", "") == webhook_url or set_telegram_webhook(decrypted_token, main_function_url, encrypted_token)
+
+    return webhook_info.get("url") == main_webhook_url or set_main_webhook()
 
 
 def is_secret_token_valid(request: Request) -> bool:
